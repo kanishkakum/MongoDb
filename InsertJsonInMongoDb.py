58096bf5139e175e094eb4d58a71ae3d12e6616e
hp <kanishkakumar408@gmail.com>
2022-08-11 01:25:48 -0700

    Json data insert into mongo db collection database

---
+++ InsertJsonInMongoDb.py
@@ -0,0 +1,78 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://kaysnow343:Viradha123@cluster0.nrxoquk.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+data1= [
+	{
+		"id": "0001",
+		"type": "donut",
+		"name": "Cake",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" },
+						{ "id": "1003", "type": "Blueberry" },
+						{ "id": "1004", "type": "Devil's Food" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5007", "type": "Powdered Sugar" },
+				{ "id": "5006", "type": "Chocolate with Sprinkles" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0002",
+		"type": "donut",
+		"name": "Raised",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0003",
+		"type": "donut",
+		"name": "Old Fashioned",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	}
+]
+
+db= client['mongoTest']
+collection = db["Json"]
+collection.insert_many(data1)
--- mongoTest.py
+++ mongoTest.py
@@ -9,6 +9,51 @@ d = {
     "mob":"3897983793"
 }
 
+e = {
+"name" : "Raj",
+     "surname" : "kumar",
+    "mob":"3897983793"
+},
+{
+"name" : "Hitesh",
+     "surname" : "kumar",
+    "mob":"3897983793"
+},
+{
+"name" : "Kanishka",
+     "surname" : "kumar",
+    "mob":"3897983793"
+}
+
+data1={
+	"id": "0001",
+	"type": "donut",
+	"name": "Cake",
+	"ppu": 0.55,
+	"batters":
+		{
+			"batter":
+				[
+					{ "id": "1001", "type": "Regular" },
+					{ "id": "1002", "type": "Chocolate" },
+					{ "id": "1003", "type": "Blueberry" },
+					{ "id": "1004", "type": "Devil's Food" }
+				]
+		},
+	"topping":
+		[
+			{ "id": "5001", "type": "None" },
+			{ "id": "5002", "type": "Glazed" },
+			{ "id": "5005", "type": "Sugar" },
+			{ "id": "5007", "type": "Powdered Sugar" },
+			{ "id": "5006", "type": "Chocolate with Sprinkles" },
+			{ "id": "5003", "type": "Chocolate" },
+			{ "id": "5004", "type": "Maple" }
+		]
+}
+
 db1 = client['mongoTest']
 coll = db1['test']
 coll.insert_one(d)
+coll.insert_many(e)
+coll.insert_many(data1)
